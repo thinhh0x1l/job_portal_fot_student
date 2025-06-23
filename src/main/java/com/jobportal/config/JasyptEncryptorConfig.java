255e356d4a27b8cbfc89e57d16305fedd2df3f2a
thinhh0x1l <hoangthinhh18.deva@gmail.com>
2025-06-23 16:55:40 +0700

    pro

--- src/main/java/com/jobportal/config/JasyptEncryptorConfig.java
+++ src/main/java/com/jobportal/config/JasyptEncryptorConfig.java
@@ -5,16 +5,21 @@ import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
 import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
 import org.jasypt.util.text.BasicTextEncryptor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class JasyptEncryptorConfig {
+
+    @Value("${JASYPT_ENCRYPTOR_PASSWORD}")
+    private String jasyptPassword;
+
     @Bean("jasyptStringEncryptor")
     public StringEncryptor stringEncryptor() {
         PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
         SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-        config.setPassword("password");
+        config.setPassword("jasyptPassword");
         config.setAlgorithm("PBEWITHHMACSHA512ANDAES_256");
         config.setKeyObtentionIterations("1000");
         config.setPoolSize("1");
